9681fd9fa89b095ff613bbd4f7a54ca5efc4c668
Kerenza13 <carlichesjunior@gmail.com>
2025-05-07 10:31:56 +0200

    primer commit vacio Carlos Morillas Delgado

---
+++ Docker-AWS/backend/src/Controller/RECUSController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class RECUSController
+{
+#[Route('/api/RECUC', name: 'api_RECUC')]
+public function index(): Response
+{
+return new Response('Symfony de Carlos Morillas Delgado Responde a la llamada de React');
+}
+}

055f105692b94cbdc7114c5d38ce7b0509b16d54
Kerenza13 <carlichesjunior@gmail.com>
2025-05-08 11:21:33 +0200

    fixex

--- Docker-AWS/backend/src/Controller/RECUCController.php
+++ Docker-AWS/backend/src/Controller/RECUCController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class RECUSController
+class RECUCController
 {
 #[Route('/api/RECUC', name: 'api_RECUC')]
 public function index(): Response

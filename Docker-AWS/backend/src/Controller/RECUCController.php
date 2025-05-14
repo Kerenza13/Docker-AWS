554195f24c47a9831e4f25b572146b3b533c79cf
Kerenza13 <carlichesjunior@gmail.com>
2025-05-14 16:02:46 +0200

    Refactor Dockerfiles and update frontend configuration for improved structure and clarity

--- Docker-AWS/backend/src/Controller/RECUCController.php
+++ Docker-AWS/backend/src/Controller/RECUCController.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace App\Controller;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class RECUCController
 {
-#[Route('/api/RECUC', name: 'api_RECUC')]
-public function index(): Response
-{
-return new Response('Symfony de Carlos Morillas Delgado Responde a la llamada de React');
-}
+    #[Route('/api/recuc', name: 'api_recuc')]
+    public function index(): Response
+    {
+        return new Response('Symfony de Carlos Morillas Delgado Responde a la llamada de React');
+    }
 }
